ec147f74a4420279cc00e10a3784c1d036114182
luiscab19 <luis19alvarez05@gmail.com>
2025-04-09 22:34:34 -0400

    feat: Se agregó graph.h

--- graph/graph.h
+++ graph/graph.h
@@ -6,6 +6,19 @@
 using namespace std;
 
 class Graph{
+    private:
+        Nodo **nodos;
+        int contador_nodos;
+        int capacidad;
+        int buscarIndiceNodo(string id);
+
+    public:
+        Graph();
+        ~Graph();
+        void agregarNodo(string id, string nombre);
+        Nodo *buscarNodo(string id);
+        void modificarNodo(string id, string nuevo_nombre);
+        void eliminarNodo(string id);
 };
 
 #endif
